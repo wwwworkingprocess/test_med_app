9664516feda9179c4bc0265de899bf87bf8365d2
Gergely Marton <wwworkingprocess@gmail.com>
2026-03-02 07:39:21 -0500

    configured mongo, backend, frontend

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:BaM22DFpa9pHthP14SivMazM@172.21.164.7:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const API_URL = window.location.hostname === "localhost" 
+    ? "https://wwworkingpro-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" 
+    : "https://wwworkingpro-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai";
